797f22d2cc688226f7391df1ac6605a3eedbac3d
infobwd <168699470+infobwd@users.noreply.github.com>
2025-09-22 00:54:48 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,8 +3,8 @@
 // Fill in your Supabase project URL and anon key below.
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const SUPABASE_URL = localStorage.getItem("APP_SUPABASE_URL") || "https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = localStorage.getItem("APP_SUPABASE_ANON_KEY") || "YOUR-ANON-KEY";
+export const SUPABASE_URL = localStorage.getItem("APP_SUPABASE_URL") || "https://jxebbwfofurrpchxzlqd.supabase.co";
+export const SUPABASE_ANON_KEY = localStorage.getItem("APP_SUPABASE_ANON_KEY") || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZWJid2ZvZnVycnBjaHh6bHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MDk4MzksImV4cCI6MjA3MzA4NTgzOX0.9tNHoDIUuMTmbI_ktmEjroMr7HE9t1F9CWh6HamleBk";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true }
